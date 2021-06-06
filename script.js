805e100f119cffec6f3a479ab895622665548a9f
amir <varg_amir@yahoo.com>
2021-06-06 15:07:32 +0430

    completed

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 var cw = $('.timeline-text').width();
 
 $('.timeline-text').css({'height':cw});
+
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.navbar-brand').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
